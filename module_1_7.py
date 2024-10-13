b0ea5c5508efde67a0a326ce184b2127cf37c310
Proshesss <djmbrovskiy@gmail.com>
2024-10-13 20:28:58 +0500

    Add files via upload

---
+++ module_1_7.py
@@ -0,0 +1,9 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+print(sorted(students))#получаю -['Aaron', 'Bilbo', 'Johnny', 'Khendrik', 'Steve']
+students=['Aaron', 'Bilbo', 'Johnny', 'Khendrik', 'Steve']#- присваиваю в алфавидном порядке
+average_grades=[sum(grades[0])/len(grades[0]),sum(grades[1])/len(grades[1]),sum(grades[2])/len(grades[2]),sum(grades[3])/len(grades[3]),sum(grades[4])/len(grades[4])]
+print(average_grades)
+grades_students={'Aaron':4.0, 'Bilbo':2.25, 'Johnny':4.0, 'Khendrik': 3.6666666666666665, 'Steve':4.8}
+print(grades_students)
+
